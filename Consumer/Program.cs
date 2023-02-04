694416df365fec00d3b886852ae8b52a9fba7aef
Mohab224466 <108239898+Mohab224466@users.noreply.github.com>
2023-02-04 14:29:52 +0200

    RMQ

---
+++ Consumer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+    
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://tgsjeyec:IWm5nKKnB-Hx1h5QVVaeY16_NoquIOf-@hummingbird.rmq.cloudamqp.com:5671/tgsjeyec");
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+// channel.QueueDeclare(queue:"letterbox", durable:false,
+// exclusive:false, autoDelete:false,arguments:null);
+
+var consumer = new EventingBasicConsumer(channel);
+// Subscribe
+consumer.Received +=(Model,ea ) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"message recieved : {message}");
+};
+
+// using (var channel2 = connection.CreateModel()){
+//     var consumer2 = new EventingBasicConsumer(channel);
+//     // Subscribe
+//     consumer.Received +=(Model,ea ) =>
+//     {
+//         var body = ea.Body.ToArray();
+//         var message = Encoding.UTF8.GetString(body);
+//         Console.WriteLine($"message recieved : {message}");
+//     };
+// }
+
+channel.BasicConsume(queue:"test_queue",autoAck:true,consumer:consumer,consumerTag:"letter");
+
+Console.ReadKey();
+
+
+
---
+++ Producer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+
+//create factory
+var factory = new ConnectionFactory();
+// factory configuration
+factory.Uri = new Uri("amqps://tgsjeyec:IWm5nKKnB-Hx1h5QVVaeY16_NoquIOf-@hummingbird.rmq.cloudamqp.com:5671/tgsjeyec");
+// factory.UserName="tgsjeyec";
+// factory.Password="IWm5nKKnB-Hx1h5QVVaeY16_NoquIOf-";
+// factory.HostName="hummingbird.rmq.cloudamqp.com";
+// create connection
+using var connection = factory.CreateConnection();
+// create channel (delivery boy)
+using var channel = connection.CreateModel();
+
+ channel.ExchangeDeclare(exchange:"test_exchange",type:"direct");
+// queue configuration 
+channel.QueueDeclare(queue:"test_queue", durable:false,
+exclusive:false, autoDelete:false,arguments:null );
+channel.QueueBind(queue:"test_queue", exchange:"test_exchange",routingKey:"test_key");
+
+// message
+var message = "This is my test message";
+
+
+// encoding message
+var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+IBasicProperties props = channel.CreateBasicProperties();
+props.ContentType="text/plain";
+
+channel.BasicPublish("test_exchange","test_key",props,encodedMessage);
+//publishing
+// channel.BasicPublish(exchange:"test_exchange",
+//                     routingKey:"test_key",
+//                     IBasicProperties:props,
+//                     body: encodedMessage );
+
+Console.WriteLine($"Published Message:{message}");
+
+// connection.Close();
